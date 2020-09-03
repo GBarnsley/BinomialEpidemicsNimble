a172757f26f1cef639b2e478ad0e7817300bf093
GBarnsley <63357847+GBarnsley@users.noreply.github.com>
2020-09-03 23:55:50 +0100

    Update model-SIR.R

--- R/model-SIR.R
+++ R/model-SIR.R
@@ -22,16 +22,8 @@ newRSIRclass <- setClass(
 )
 #' Creates an object of class SIR, iSIR or rSIR
 #'
-#' Takes any combination of the values in the SIR model and uses dataGen to generate
-#' unspecified data where it can. If S, I and newI but R and newR are then it will
-#' generate an object of class iSIR that inherits from the SIR class. This class functions
-#' the same for most functions other than in the metropolis hasting algorithm where it treats
-#' newI as a parameter to generate. if R, I and newR are missing then it will make an object of
-#' class rSIR which treats newR as a parameter.
+#' Generates and compiles SIR epidemic model.
 #'
-#' @param S Time series of the Susceptible Population
-#' @param I Time series of the Infectious Population
-#' @param R Time series of the Recovered Population
 #' @param newI Time series of new infections
 #' @param newR Time series of new recoveries
 #' @param N The size of the population
@@ -39,10 +31,7 @@ newRSIRclass <- setClass(
 #' @param t.step The amount of time in each step of the model, actual value depends on the units of Beta and Gamma
 #' @return Object of class SIR, iSIR or rSIR which will contain the compiled nimble model
 #' @export
-SIR <- function(S = NULL,
-                I = NULL,
-                R = NULL,
-                newI = NULL,
+SIR <- function(newI = NULL,
                 newR = NULL,
                 N = NULL,
                 t.step = 1,
@@ -51,33 +40,6 @@ SIR <- function(S = NULL,
   if(is.null(N)){
     print("Error: N must be specified")
     return(NA)
-  }
-  if(is.null(S)&!is.null(newI)){
-    S <- rep(N-1, length(newI) + 1)
-    for(i in 2:length(S)){
-      S[i] <- N - 1 - sum(newI[1:(i-1)])
-    }
-  }
-  if(is.null(R)&!is.null(newR)){
-    R <- rep(0, length(newR) + 1)
-    for(i in 2:length(R)){
-      R[i] <- sum(newR[1:(i-1)])
-    }
-  }
-  if(is.null(I)&!is.null(S)&!is.null(R)){
-    I <- N - S - R
-  }
-  if(is.null(newI)&!is.null(S)){
-    newI <- -diff(S)
-  }
-  if(is.null(newR)&!is.null(R)){
-    newR <- diff(R)
-  }
-  if(Frequency){
-    Frequency <- 1
-  }
-  else{
-    Frequency <- 0
   }
   #Setting up models based on nulls
   tempCode <- nimbleCode({
@@ -111,7 +73,7 @@ SIR <- function(S = NULL,
                       Frequency = Frequency),
           inits = list(Beta = 1,
                        Gamma = 1,
-                       newI = rep(0, length(newR))
+                       newI = c(sum(newR) - 1, rep(0, length(newR) - 1))
           ),
           calculate = FALSE
         )
@@ -137,7 +99,7 @@ SIR <- function(S = NULL,
                       Frequency = Frequency),
           inits = list(Beta = 1,
                        Gamma = 1,
-                       newR = rep(0, length(newI))
+                       newR = c(rep(0, length(newI) - 1), sum(newI) + 1)
           ),
           calculate = FALSE
         )
