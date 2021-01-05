7cdb61a230c94b3c4b73200dabd1cd38af7eb80b
GBarnsley <63357847+GBarnsley@users.noreply.github.com>
2021-01-05 12:21:15 +0000

    Update general-Probability Function.R

--- R/general-Probability Function.R
+++ R/general-Probability Function.R
@@ -25,7 +25,7 @@ probGen <- nimble::nimbleFunction(
 #' @param x some non-negative values
 #' @return a set of values on the range (0,1) that sum to 1
 #' @export
-multiProbGen <- <- nimbleFunction(run = function(x=double(1)){
+multiProbGen <- nimbleFunction(run = function(x=double(1)){
   returnType(double(1))
   failValue <- sum(x)
   x <- c(probGen(x),exp(-failValue))
